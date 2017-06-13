0a36b047c01bde3f5e20c236ae8f0a2289110072
markusfluer <fluer.markus@googlemail.com>
2017-06-13 12:30:43 +0200

    Created the first working draft of a folder generator. Filegenerator for config and placeholder files will follow

--- generators/createFolderStructure.js
+++ generators/createFolderStructure.js
@@ -1,1 +1,43 @@
-'mkdir controllers helpers language language/en-GB language/de-DE models models/fields models/forms sql sql/updates sql/updates/mysql tables views'
+//'mkdir controllers helpers language language/en-GB language/de-DE models models/fields models/forms sql sql/updates sql/updates/mysql tables views'
+/**
+ * Creates folders and simple empty files for any type of joomla extension
+ */
+
+"use strict";
+const
+    _ = require('lodash'),
+    fs = require('fs-extra'),
+    path = require('path'),
+    log = require('../utility/logSys.js'),
+    parseFolder = function (folderStructure, key) {
+        let folderList = [];
+        folderList.push(key);
+        _.each(folderStructure, function (value, key) {
+            if (key == 'files') { return true; }
+            folderList = _.concat(folderList, parseFolder(folderStructure[key], key));
+        });
+        return _.map(folderList, function (item, i) {
+            return path.join(key, item);
+        });
+    },
+    parseFolderList = function (folderStructure) {
+        let folderList = [];
+        _.each(folderStructure, function (value, key) {
+            folderList = _.concat(folderList, parseFolder(folderStructure[key], key));
+        });
+        console.log('folderList=>');
+        console.dir(folderList, { color: true, depth: 5 });
+        return folderList;
+    },
+    createFolderStructure = function (globals) {
+        const folderTarget = "../" + globals.config.type,
+            folderList = parseFolderList(globals.config.templateConf.base);
+        log.warn('Generating folder structure.');
+        _.each(folderList, function (folder, iterator) {
+            let folderPath = path.join(folderTarget, folder);
+            fs.mkdirp(folderPath);
+            log.debug("generated folder" + folderPath);
+        })
+    };
+
+module.exports = createFolderStructure;
